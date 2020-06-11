3c97f99f45b5618867b5b705fe78f7f16fef5674
baiyundou <qwe6588038>
2020-06-11 18:49:29 +0800

    h

---
+++ rpc-all/src/main/java/com/wang/rpcall/rpckids/common/IMessageHandler.java
@@ -0,0 +1,10 @@
+package com.wang.rpcall.rpckids.common;
+
+import io.netty.channel.ChannelHandlerContext;
+
+@FunctionalInterface
+public interface IMessageHandler<T> {
+
+	void handle(ChannelHandlerContext ctx, String requestId, T message);
+
+}
